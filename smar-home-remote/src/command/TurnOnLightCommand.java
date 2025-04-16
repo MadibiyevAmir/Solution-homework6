7c1d8588e7be7838e0c9672cc345785df16a305c
MadibiyevAmir <amir.madibiyev@narxoz.kz>
2025-04-16 18:13:52 +0500

    TurnOnLightCommand.java

---
+++ smar-home-remote/src/command/TurnOnLightCommand.java
@@ -0,0 +1,21 @@
+package command;
+
+import device.Light;
+
+public class TurnOnLightCommand implements Command {
+    private Light light;
+
+    public TurnOnLightCommand(Light light) {
+        this.light = light;
+    }
+
+    @Override
+    public void execute() {
+        light.turnOn();
+    }
+
+    @Override
+    public void undo() {
+        light.turnOff();
+    }
+}
