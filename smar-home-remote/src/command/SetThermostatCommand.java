3effe662d212d581d4f8b27f4a78841058335ace
MadibiyevAmir <amir.madibiyev@narxoz.kz>
2025-04-16 18:14:13 +0500

    SetThermostatCommand.java

---
+++ smar-home-remote/src/command/SetThermostatCommand.java
@@ -0,0 +1,23 @@
+package command;
+
+import device.Thermostat;
+
+public class SetThermostatCommand implements Command {
+    private Thermostat thermostat;
+    private int temperature;
+
+    public SetThermostatCommand(Thermostat thermostat, int temperature) {
+        this.thermostat = thermostat;
+        this.temperature = temperature;
+    }
+
+    @Override
+    public void execute() {
+        thermostat.setTemperature(temperature);
+    }
+
+    @Override
+    public void undo() {
+        thermostat.revertTemperature();
+    }
+}
